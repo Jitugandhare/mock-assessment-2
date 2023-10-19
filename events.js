54b2426c39751919745d94a399bd373bc6ee0a82
jitu gandhare <jitugandhare@gmail.com>
2023-10-19 19:44:24 +0530

    mock assesment 1

--- events.js
+++ events.js
@@ -9,16 +9,33 @@ function eventCards(events) {
     eventCard.innerHTML = `
             <img src="${ele.poster}" alt="${ele.name}">
             <h3>${ele.name}</h3>
-            <p>Date: ${ele.date}</p>
-            <p>Location: ${ele.location}</p>
-            <p>Category: ${ele.category}</p>
-            <p>Price: $${ele.price}</p>
+            <h5>Date: ${ele.date}</h5>
+            <h5>Location: ${ele.location}</h5>
+            <h5>Category: ${ele.category}</h5>
+            <h5>Price: $${ele.price}</h5>
         `;
     e_card.appendChild(eventCard);
   });
 }
 
-
+document.getElementById("cFilter").addEventListener("change", function () {
+  const selectedCategory = this.value;
+  if (selectedCategory === "All") {
+    fetch(`${apiUrl}/events`)
+      .then((response) => response.json())
+      .then((data) => {
+        eventCards(data);
+      })
+      .catch((error) => console.error(error));
+  } else {
+    fetch(`${apiUrl}/events?category=${selectedCategory}`)
+      .then((response) => response.json())
+      .then((data) => {
+        eventCards(data);
+      })
+      .catch((error) => console.error(error));
+  }
+});
 
 document.getElementById("pricesort").addEventListener("change", function () {
   const sortDirection = this.value;
